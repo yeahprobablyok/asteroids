24cafef725bdf84fe6e184860a7258efcdb45d1a
Richard Koopmans <richardkoopmans12@gmail.com>
2026-07-03 14:27:27 +1000

    Added Sprites + FPS Clock

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -15,6 +18,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+    
     
     screen.fill("black")
 
